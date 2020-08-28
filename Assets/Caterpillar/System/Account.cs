b56cfb84b677351eca948797ea16361fc38cf7b1
fabsther <fabsther@gmail.com>
2020-08-28 00:52:32 -0700

    Added google play loggin
    maj goog le play

--- Assets/Caterpillar/System/Account.cs
+++ Assets/Caterpillar/System/Account.cs
@@ -3,40 +3,102 @@ using PlayFab.ClientModels;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+#if UNITY_ANDROID
+// These using statements are required.
+using GooglePlayGames;
+using GooglePlayGames.BasicApi;
+#endif
 #if UNITY_EDITOR || UNITY_STANDALONE_WIN
 //using Steamworks;
 #endif
 
 public class Account : MonoBehaviour
 {
-    public object SteamUser { get; private set; }
+    public UnityEvent LoginSuccess;
+    public UnityEvent Loginfail;
+    public TMPro.TextMeshProUGUI Output;
+
+    //public object SteamUser { get; private set; }
 
     // Start is called before the first frame update
     void Start()
     {
+        DontDestroyOnLoad(gameObject);
 #if UNITY_EDITOR || UNITY_STANDALONE_WIN
         //var request = new LoginWithSteamRequest() { CreateAccount = true };
         //PlayFabClientAPI.LoginWithSteam(request, OnLoginSuccess, OnLoginFailure);
 #elif UNITY_ANDROID
-        var request = new LoginWithAndroidDeviceIDRequest() { AndroidDeviceId = SystemInfo.deviceUniqueIdentifier, CreateAccount = true };
-        PlayFabClientAPI.LoginWithAndroidDeviceID(request, OnLoginSuccess, OnLoginFailure);
+        // The following grants profile access to the Google Play Games SDK.
+        // Note: If you also want to capture the player's Google email, be sure to add
+        // .RequestEmail() to the PlayGamesClientConfiguration
+        PlayGamesClientConfiguration config = new PlayGamesClientConfiguration.Builder()
+        .AddOauthScope("profile")
+        .RequestServerAuthCode(false)
+        .Build();
+        PlayGamesPlatform.InitializeInstance(config);
+
+        // recommended for debugging:
+        PlayGamesPlatform.DebugLogEnabled = true;
+
+        // Activate the Google Play Games platform
+        PlayGamesPlatform.Activate();
 #elif UNITY_IOS
         var request = new LoginWithIOSDeviceIDRequest() { DeviceId = SystemInfo.deviceUniqueIdentifier, CreateAccount = true };
         PlayFabClientAPI.LoginWithIOSDeviceID(request, OnLoginSuccess, OnLoginFailure);
 #endif
+        OnSignInButtonClicked();
     }
 
-    private void OnLoginSuccess(LoginResult result)
+#if UNITY_ANDROID
+
+    public void OnSignInButtonClicked()
     {
-        Debug.Log("Congratulations, you made your first successful API call!");
+        Social.localUser.Authenticate(OnAuthentificate);
     }
 
-    private void OnLoginFailure(PlayFabError error)
+    private void OnAuthentificate(bool Success)
     {
-        Debug.LogWarning("Something went wrong with your first API call.  :(");
-        Debug.LogError("Here's some debug information:");
-        Debug.LogError(error.GenerateErrorReport());
+        if (Success)
+        {
+            Debug.Log("Google : Signed In");
+            Output.text = "Google : Signed In";
+
+            var serverAuthCode = PlayGamesPlatform.Instance.GetServerAuthCode();
+            Debug.Log("Google : Server Auth Code: " + serverAuthCode);
+
+            LoginWithGoogleAccountRequest LoginRequest = new LoginWithGoogleAccountRequest()
+            {
+                TitleId = PlayFabSettings.TitleId,
+                ServerAuthCode = serverAuthCode,
+                CreateAccount = true
+            };
+
+            PlayFabClientAPI.LoginWithGoogleAccount(LoginRequest, OnPlayfabLoginResult, OnPlayfabLoginError);
+        }
+        else
+        {
+            Loginfail.Invoke();
+            Debug.Log("Google : Failed to Authorize your login");
+            Output.text = "Google : Failed to Authorize your login";
+        }
     }
+
+    private void OnPlayfabLoginResult(LoginResult Result)
+    {
+        Debug.Log("Playfab : Signed In as " + Result.PlayFabId);
+        LoginSuccess.Invoke();
+        Output.text = "Playfab : Signed In as " + Result.PlayFabId;
+    }
+
+    private void OnPlayfabLoginError(PlayFabError Error)
+    {
+        Debug.Log("Playfab : Error : " + Error.ToString());
+        Loginfail.Invoke();
+        Output.text = "Playfab : Error : " + Error.ToString();
+    }
+
+#endif
 
 #if UNITY_EDITOR || UNITY_STANDALONE_WIN
     //public string GetSteamAuthTicket()
