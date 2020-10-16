00cbcbcd44812aa8af00d0c49ffe4a5649c06c50
fabsther <fabsther@gmail.com>
2020-10-16 16:58:30 -0700

    Clean up + update niveaux
    update niveau 1 a 5
    Prefabage niveau 4
    Clean up des assets sample des plugins

---
+++ Assets/Caterpillar/System/Account_PC.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+using PlayFab;
+using PlayFab.ClientModels;
+using System;
+
+#if UNITY_STANDALONE_WIN || (UNITY_EDITOR && !UNITY_ANDROID)
+
+public partial class Account : MonoBehaviour
+{
+    #region Properties
+
+    private bool LoggedWithEmail = false;
+
+    #endregion
+
+    #region Login
+
+    void InitializeLogin()
+    {
+        SignInWithEmail();
+    }
+
+    private void SignInWithEmail()
+    {
+        if (LoggedWithEmail == false)
+        {
+            LoginWithEmailAddressRequest LoginRequest = new LoginWithEmailAddressRequest()
+            {
+                TitleId = PlayFabSettings.TitleId,
+                Email = "test@test.com",
+                Password = "password"
+            };
+
+            PlayFabClientAPI.LoginWithEmailAddress(LoginRequest, OnPlayfabEmailLoginSuccess, OnPlayfabEmailLoginError);
+        }
+    }
+
+    private void OnPlayfabEmailLoginSuccess(LoginResult Result)
+    {
+        OnPlayfabLoginSuccess(Result);
+        LoggedWithEmail = true;
+        Debug.LogWarning("Playfab : Success to Login with TEST Email !");
+    }
+
+    private void OnPlayfabEmailLoginError(PlayFabError Error)
+    {
+        Debug.LogError("Playfab PC Device Login Failed -> Error : " + Error.ToString());
+    }
+
+    #endregion
+
+    #region Notifications
+
+    public static void SetNotificationActive(bool Active)
+    {
+    }
+
+    public static bool GetNotificationEnabled()
+    {
+        return false;
+    }
+
+    #endregion
+}
+
+#endif
